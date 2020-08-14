c368d58b89a44f1f1143cdba661912e23d8bcdb7
AlinaYu <ylq1349578685@163.com>
2020-08-14 16:48:25 +0800

    添加文件

---
+++ helloGit/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2020/8/14 16:41
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
